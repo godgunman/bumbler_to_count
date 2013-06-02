e3137a91b7ec9a71a991bd0480601baf430a4602
telgniw <b97902044@ntu.edu.tw>
2013-06-02 11:40:11 +0800

    Update audio time mark.

--- web/js/constants.js
+++ web/js/constants.js
@@ -1,66 +1,90 @@
   var MAP = {
     '0': {
       'start': 0,
-      'duration': 1
+      'duration': 0
     },
     '1': {
-      'start': 0,
-      'duration': 1
+      'start': 0.18,
+      'duration': 0.50
     },
     '2': {
-      'start': 1,
-      'duration': 1
+      'start': 0.69,
+      'duration': 0.50
     },
     '3': {
-      'start': 2,
-      'duration': 1
+      'start': 1.35,
+      'duration': 0.52
     },
     '4': {
-      'start': 3,
-      'duration': 1
+      'start': 2.06,
+      'duration': 0.50
     },
     '5': {
-      'start': 3,
-      'duration': 1
+      'start': 2.82,
+      'duration': 0.52
     },
     '6': {
-      'start': 3,
-      'duration': 1
+      'start': 3.64,
+      'duration': 0.52
     },
     '7': {
-      'start': 3,
-      'duration': 1
+      'start': 4.51,
+      'duration': 0.50
     },
     '8': {
-      'start': 3,
-      'duration': 1
+      'start': 5.34,
+      'duration': 0.50
     },
     '9': {
-      'start': 3,
-      'duration': 1
+      'start': 6.27,
+      'duration': 0.50
     },
     '10': {
-      'start': 3,
-      'duration': 1
+      'start': 6.88,
+      'duration': 0.60
+    },
+    '2-2': {
+      'start': 7.92,
+      'duration': 0.34
+    },
+    '100': {
+      'start': 8.25,
+      'duration': 0.20
+    },
+    '1000': {
+      'start': 8.45,
+      'duration': 0.39
+    },
+    '10000': {
+      'start': 8.84,
+      'duration': 0.20
+    },
+    '100m': {
+      'start': 9.02,
+      'duration': 0.20
     },
     '+': {
-      'start': 3,
-      'duration': 1
+      'start': 9.23,
+      'duration': 0.25
     },
     '-': {
-      'start': 3,
-      'duration': 1
-    },
-    '/': {
-      'start': 3,
-      'duration': 1
+      'start': 9.48,
+      'duration': 0.27
     },
     '*': {
-      'start': 3,
-      'duration': 1
+      'start': 9.75,
+      'duration': 0.35
+    },
+    '/': {
+      'start': 10.12,
+      'duration': 0.26
     },
     '=': {
-      'start': 3,
-      'duration': 1
+      'start': 10.37,
+      'duration': 0.60
+    },
+    'thanks': {
+      'start': 11.07,
+      'duration': 0.80
     },
  };
